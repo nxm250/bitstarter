5ff9e77b8f394a61edeb7ac60e071071025b5076
Nimit Mehta <nimitm@gmail.com>
2013-09-01 09:08:47 +0000

    made changes to web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+  response.send(infile);
 });
 
 var port = process.env.PORT || 5000;
