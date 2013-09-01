f22de76634b476e7e474038f8fb025272f62e16e
Nimit Mehta <nimitm@gmail.com>
2013-09-01 09:01:23 +0000

    made changes to web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
+var infile = fs.readFileSync('index.html', "utf-8");
+var buffer = new Buffer(infile, "utf-8");
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('buffer');
 });
 
 var port = process.env.PORT || 5000;
