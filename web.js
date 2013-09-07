668ce5ff14f0d0ecd91f5bdc461f72682d2c0efd
Nimit Mehta <nimitm@gmail.com>
2013-09-07 08:43:17 +0000

    Modified Web.js

--- web.js
+++ web.js
@@ -1,17 +1,16 @@
 var fs = require('fs');
 var express = require('express');
-var infile = fs.readFileSync('index.html', "utf-8");
-var buffer = new Buffer(infile);
-buffer.write(infile);
+var htmlfile = "index.html";
 
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+ var html=fs.readFileSync(htmlfile).toString();  
+response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
